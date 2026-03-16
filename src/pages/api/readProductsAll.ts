8bbba6975fcddcbd0ebfcbcc493499ad5a3e395f
patatita <gabrielpulidobarrera@gmail.com>
2026-03-16 20:52:59 +0100

    changed the route for slug, finished the UI for the table almost fully.

---
+++ src/pages/api/readProductsAll.ts
@@ -0,0 +1,40 @@
+import type { APIRoute } from 'astro';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+import { PrismaClient } from '../../generated/prisma/client';
+export const prerender = false;
+
+const adapter = new PrismaMariaDb({
+  host: "localhost",
+  port: 3306,
+  user: "dwes",
+  password: "dwes",
+  database: "dwes",
+});
+const prisma = new PrismaClient({ adapter });
+
+export const POST: APIRoute = async function() {
+  try {
+
+
+    const result = await prisma.product.findMany({
+
+    });
+
+    return new Response(
+      JSON.stringify({
+        success: true,
+        message: 'Data read successfully',
+        data: result
+      }),
+      { status: 200, headers: { 'Content-Type': 'application/json' } }
+    );
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        success: false,
+        message: 'Invalid request body or database error'
+      }),
+      { status: 400, headers: { 'Content-Type': 'application/json' } }
+    );
+  }
+};
