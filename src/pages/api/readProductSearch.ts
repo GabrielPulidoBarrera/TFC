8d18d59eef3ef64d64360144f0d28b244cdfa03e
patatita <gabrielpulidobarrera@gmail.com>
2026-03-22 23:24:43 +0100

    added search function

---
+++ src/pages/api/readProductSearch.ts
@@ -0,0 +1,46 @@
+import type { APIRoute } from 'astro';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+import { PrismaClient } from '../../generated/prisma/client';
+export const prerender = false;
+
+const adapter = new PrismaMariaDb({
+  host: "localhost",
+  port: 3306,
+  user: "dwes",
+  password: "dwes",
+  database: "dwes",
+});
+const prisma = new PrismaClient({ adapter });
+
+export const POST: APIRoute = async function({ request }) {
+  try {
+    const { text } = await request.json();
+
+    console.log(text)
+
+    const result = await prisma.product.findMany({
+      where: {
+        name: {
+          contains: text   
+        }
+      }
+    });
+
+    return new Response(
+      JSON.stringify({
+        success: true,
+        message: 'Data read successfully',
+        data: result
+      }),
+      { status: 200, headers: { 'Content-Type': 'application/json' } }
+    );
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        success: false,
+        message: 'Invalid request body or database error'
+      }),
+      { status: 400, headers: { 'Content-Type': 'application/json' } }
+    );
+  }
+};
