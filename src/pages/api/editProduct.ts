3139c89ea89dd4c371db8bfa615654e4790aeb88
patatita <44934078+patatitta@users.noreply.github.com>
2026-04-29 10:32:30 +0200

    added function to edit name of product

---
+++ src/pages/api/editProduct.ts
@@ -0,0 +1,62 @@
+import type { APIRoute } from 'astro';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb'
+import { PrismaClient } from '../../generated/prisma/client'
+import { conectar } from '@libs/database/adapter';
+export const prerender = false
+
+const prisma = await conectar()
+
+
+export const POST: APIRoute = async function({ request }){
+  try {
+    let {id, name, cookie} = await request.json();
+    console.log(name)
+
+    console.log("COOKIE!!!"+ id)
+
+    let data: any = {} 
+
+
+      if (name !== undefined) {
+      data.name = name;
+    }
+    
+
+
+
+    const result = await prisma.product.update({
+      where: {
+        id: id,
+        userID: cookie,
+      },
+      data
+    });
+    
+    console.log("Collection edited:", result);
+    
+    return new Response(
+      JSON.stringify({ 
+        success: true, 
+        message: 'Collection editing successfully',
+        data: result 
+      }),
+      { 
+        status: 200, 
+        headers: { 'Content-Type': 'application/json' } 
+      }
+    );
+  } catch (error: any) {
+    console.error("Error editing collection: ", error);
+    
+    return new Response(
+      JSON.stringify({ 
+        error: 'Error editing collection',
+        details: error.message 
+      }),
+      { 
+        status: 500, 
+        headers: { 'Content-Type': 'application/json' } 
+      }
+    );
+  }
+}
