fad8811e63b0d88be6b2c2f6c3d28c546fb6801f
patatita <gabrielpulidobarrera@gmail.com>
2026-05-04 13:15:25 +0200

    added SQL scripts for the creation of the database

---
+++ SQL/crearUsuarioDWES.sql
@@ -0,0 +1,3 @@
+-- Create user dwes with password dwes and all privileges
+
+GRANT ALL PRIVILEGES ON *.* TO `dwes`@`localhost` IDENTIFIED BY PASSWORD '*559CC1EBF0E7C1F7A4EEDE8093C3F69A316DACB4' WITH GRANT OPTION;
