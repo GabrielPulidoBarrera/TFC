fb24d78596384bdfe7895f798b4a4a2a30cdb4c4
patatita <44934078+patatitta@users.noreply.github.com>
2026-05-06 12:00:01 +0200

    changed name of borrarCookie to conform with current structure, added a delete collection function

---
+++ src/pages/api/deleteCollection.ts
@@ -0,0 +1,33 @@
+import type { APIRoute } from 'astro';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb'
+import { PrismaClient } from '../../generated/prisma/client'
+import { conectar } from '@libs/database/adapter';
+export const prerender = false
+
+const prisma = await conectar()
+
+
+export const POST: APIRoute = async function({ request }) {
+  
+  const { idLink } = await request.json();
+
+    console.log("SEVIDORrrr    "+idLink)
+
+  const result = await prisma.collection.delete({
+     where: {
+        id: idLink
+
+      }
+  })
+    return new Response(
+      JSON.stringify({ 
+        success: true, 
+        message: 'Collection deleted successfully',
+        data: result 
+      }),
+      { 
+        status: 200, 
+        headers: { 'Content-Type': 'application/json' } 
+      }
+    );
+}
